76c6f2d84f3f444fa70c32beb8f51ffe06e370c4
Aisha Yusuf <goldeous4@gmail.com>
2026-02-17 10:30:51 +0100

    Refactor static web app module and update outputs for Azure Static Web App

--- modules/static-web-app/main.tf
+++ modules/static-web-app/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_static_site" "example" {
+resource "azurerm_static_web_app" "example" {
   name                = var.app_name
   resource_group_name = var.resource_group_name
   location            = var.location
--- modules/static-web-app/outputs.tf
+++ modules/static-web-app/outputs.tf
@@ -1,21 +1,20 @@
 output "default_host_name" {
-  value       = azurerm_static_site.example.default_host_name
+  value       = azurerm_static_web_app.example.default_host_name
   description = "Default hostname of the static web app"
 }
 
 output "static_site_url" {
-  value       = "https://${azurerm_static_site.example.default_host_name}"
+  value       = "https://${azurerm_static_web_app.example.default_host_name}"
   description = "Full URL of the static web app"
 }
 
 output "static_site_id" {
-  value       = azurerm_static_site.example.id
+  value       = azurerm_static_web_app.example.id
   description = "Resource ID of the static web app"
 }
 
 output "api_key" {
-  value       = azurerm_static_site.example.api_key
+  value       = azurerm_static_web_app.example.api_key
   description = "API key for deploying to the static web app"
   sensitive   = true
 }
-
